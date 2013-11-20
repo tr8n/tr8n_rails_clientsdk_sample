fb9b7511a943d75d86e78979e343eb5e25a8d62d
Michael Berkovich <theiceberk@gmail.com>
2013-11-19 19:20:51 -0800

    minor fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,22 +19,22 @@ ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
-namespace :deploy do
-  namespace :assets do
-    desc "Precompile assets locally and then rsync to app servers"
-    task :precompile, :only => { :primary => true } do
-      if skip_assets
-        logger.info "Skipping asset pre-compilation"
-      else
-        run_locally "mkdir -p public/__assets; mv public/__assets public/assets;"
-        run_locally "bundle exec rake assets:clean_expired; bundle exec rake assets:precompile;"
-        servers = find_servers :roles => [:app], :except => { :no_release => true }
-        servers.each do |server|
-          run_locally "rsync -av ./public/assets/ #{user}@#{server}:#{shared_path}/assets/;"
-        end
-        run_locally "mv public/assets public/__assets"
-        # run "ln -s #{shared_path}/assets #{fetch(:latest_release)}/public/assets"
-      end
-    end
-  end
-end
+#namespace :deploy do
+#  namespace :assets do
+#    desc "Precompile assets locally and then rsync to app servers"
+#    task :precompile, :only => { :primary => true } do
+#      if skip_assets
+#        logger.info "Skipping asset pre-compilation"
+#      else
+#        run_locally "mkdir -p public/__assets; mv public/__assets public/assets;"
+#        run_locally "bundle exec rake assets:clean_expired; bundle exec rake assets:precompile;"
+#        servers = find_servers :roles => [:app], :except => { :no_release => true }
+#        servers.each do |server|
+#          run_locally "rsync -av ./public/assets/ #{user}@#{server}:#{shared_path}/assets/;"
+#        end
+#        run_locally "mv public/assets public/__assets"
+#        # run "ln -s #{shared_path}/assets #{fetch(:latest_release)}/public/assets"
+#      end
+#    end
+#  end
+#end
